fa736805d506c3eb649b8e5611d3b02972bd06c2
Breno <bcaua205@gmail.com>
2022-12-14 20:06:04 -0300

    Add Entity User

--- src/Commerce.Domain/Entities/User.cs
+++ src/Commerce.Domain/Entities/User.cs
@@ -4,5 +4,5 @@ public class User : BaseEntity
     public string Name { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
-    public string PassWord { get; set; }
+    public string Password { get; set; }
 }
