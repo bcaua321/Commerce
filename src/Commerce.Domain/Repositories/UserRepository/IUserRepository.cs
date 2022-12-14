8272e46f064c45172054fd21a1476f32d8800d8e
Breno <bcaua205@gmail.com>
2022-12-14 20:06:33 -0300

    Replace IUserRepository

--- src/Commerce.Domain/Repositories/UserRepository/IUserRepository.cs
+++ src/Commerce.Domain/Repositories/UserRepository/IUserRepository.cs
@@ -1,5 +1,6 @@
 using Entities;
 
+namespace Repositories.UserRepository;
 public interface IUserRepository 
 {
     public Task<User> Create(User user);
@@ -7,4 +8,5 @@ public interface IUserRepository
     public Task<List<User>> GetAlls();
     public Task<User> GetById(int id);
     public Task<bool> ExistsWithId(int id);
+    public Task DeleteById(int id);
 }
